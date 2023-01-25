d3c138d7c9ccc4d38bd21708a60302eb72a1ee43
Jacounpo <jacopomilanesi.4@gmail.com>
2023-01-25 14:50:18 +0100

    Create index html along witch index js. Create first skeleton of the wallet model.

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+(function() {
+    var wallet;
+    function addOperation() {
+
+    }
+    function removeOperation() {
+
+    }
+    function findOperation() {
+
+    }
+    function getBalance() {
+    return wallet.getBalance();
+
+    }
+    function getOperation() {
+    return wallet.getOperation();
+
+    }
+
+    document.addEventListener('DOMContentLoaded',function() {
+    wallet = new Wallet();  
+    });
+})();
---
+++ src/js/models/Wallet.js
@@ -0,0 +1,17 @@
+function Wallet() {
+    this.addOperation = function() {
+
+    }
+    this.removeOperation = function() {
+    
+    }
+    this.findOperation = function() {
+    
+    }
+    this.getBalance = function() {
+    
+    }
+    this.getOperations = function() {
+        
+    }
+}
